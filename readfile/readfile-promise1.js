0f5513c14db932e0d2cc19343080dc64c9fd6684
Alan <0527alan.lin@gmail.com>
2022-05-15 13:01:47 +0800

    edit

---
+++ readfile/readfile-promise1.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+let p = new Promise((resolve, reject) => {
+  fs.readFile("test.txt", "utf-8", (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(data);
+    }
+  });
+});
+
+p.then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(`喔喔喔，發生錯誤了`);
+  console.error(error);
+});
