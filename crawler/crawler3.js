b2bc93372d6963f1e57e96fd78382afaafcc1340
Alan <0527alan.lin@gmail.com>
2022-05-17 16:30:19 +0800

    good

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,4 +1,28 @@
-
 // await version
 // 1. read stock no from file (fs)
 // 2. axios.get to request data
+
+const axios = require('axios');
+const fs = require("fs/promises");
+
+(async () => {
+  let stockNo = await fs.readFile("stock.txt", "utf-8");
+//   console.log("read stock no from file:", stockNo);
+  // https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220301&stockNo=2330
+  axios
+    .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        // 設定 query string
+        response: "json",
+        date: "20220301",
+        stockNo: stockNo,
+      },
+    })
+    .then((response) => {
+      // response 物件
+      console.log(response.data);
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+})();
