a3495713a073cd19103ba5e885fc916ef4cb9344
Alan <0527alan.lin@gmail.com>
2022-05-21 10:38:40 +0800

    go

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -2,27 +2,29 @@
 // 1. read stock no from file (fs)
 // 2. axios.get to request data
 
-const axios = require('axios');
-const fs = require("fs/promises");
+//npm i axios 因為這是第三方模組
+const axios = require("axios");
+//fs(filesystem) 內建的模組不用安裝，可以直接使用
+const fs = require("fs/promises"); // ->使用promise版本
 
 (async () => {
-  let stockNo = await fs.readFile("stock.txt", "utf-8");
-//   console.log("read stock no from file:", stockNo);
-  // https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220301&stockNo=2330
-  axios
-    .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-      params: {
-        // 設定 query string
-        response: "json",
-        date: "20220301",
-        stockNo: stockNo,
-      },
-    })
-    .then((response) => {
-      // response 物件
-      console.log(response.data);
-    })
-    .catch((e) => {
-      console.error(e);
-    });
+  try {
+    let stockNo = await fs.readFile("stock.txt", "utf-8");
+    //   console.log("read stock no from file:", stockNo);
+    // https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220301&stockNo=2330
+    let response = await axios.get(
+      "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+      {
+        params: {
+          // 設定 query string
+          response: "json",
+          date: "20220301",
+          stockNo: stockNo,
+        },
+      }
+    );
+    console.log(response.data);
+  } catch (e) {
+    console.error(e);
+  }
 })();
