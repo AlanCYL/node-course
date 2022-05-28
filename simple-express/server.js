daf724a54596ad895493139f1a588c37fa49edf5
Alan <0527alan.lin@gmail.com>
2022-05-28 17:02:12 +0800

    hw

--- simple-express/server.js
+++ simple-express/server.js
@@ -4,23 +4,24 @@ const express = require("express");
 
 // 利用 express 來建立一個 express application
 const app = express();
-
+const cors = require("cors");
+app.use(cors());
 // const path = require("path");
 
 const mysql = require("mysql2");
-require('dotenv').config();
+require("dotenv").config();
 
-let pool =mysql.createPool({
-  host: process.env.DB_host,
-  port: process.env.DB_port,
-  user: process.env.DB_user,
-  password: process.env.DB_password,
-  database: process.env.DB_database,
-  // 為了 pool 新增的參數
-  connectionLimit: 10,
-})
-.promise();
-
+let pool = mysql
+  .createPool({
+    host: process.env.DB_host,
+    port: process.env.DB_port,
+    user: process.env.DB_user,
+    password: process.env.DB_password,
+    database: process.env.DB_database,
+    // 為了 pool 新增的參數
+    connectionLimit: 10,
+  })
+  .promise();
 
 //express 是一個由 middleware (中間件) 組成的世界
 
@@ -42,20 +43,22 @@ let pool =mysql.createPool({
 //method:get, post, put, delete, .....
 app.get("/ssr", (request, response, next) => {
   //送回response，結束了 request-response cycle
-  response.render('index', {
-    stocks: ['台積電', '長榮', '聯發科'],
+  response.render("index", {
+    stocks: ["台積電", "長榮", "聯發科"],
   });
 });
 
 app.get("/stocks", async (request, response, next) => {
-  let [data, fields] = await pool.execute('SELECT * FROM stocks');
+  let [data, fields] = await pool.execute("SELECT * FROM stocks");
   response.json(data);
 });
 
 app.get("/stocks/:stockId", async (request, response, next) => {
-  let [data, fields] = await pool.execute('SELECT * FROM stocks WHERE id=' + request.params.stockId);
+  let [data, fields] = await pool.execute(
+    "SELECT * FROM stocks WHERE id=" + request.params.stockId
+  );
 
-  console.log('query stock by id:', data);
+  console.log("query stock by id:", data);
   //空資料(查不到資料)有兩種處理方式:
   //1. 200 OK 就回 []
   //2. 回覆 404
@@ -65,7 +68,6 @@ app.get("/stocks/:stockId", async (request, response, next) => {
   } else {
     response.json(data);
   }
-
 });
 
 //這個中間件在所有的路由後面
