b5267bd58215876006a9c163b524c9b49fdf6eb6
Alan <0527alan.lin@gmail.com>
2022-05-28 17:20:41 +0800

    21111

--- stock-fe/src/components/Stock.js
+++ stock-fe/src/components/Stock.js
@@ -11,22 +11,23 @@ const Stock = () => {
     };
     getStocks();
   }, []);
-  return stocks.map((stock) => {
-    return (
-      <div>
-        <h2 className="ml-7 mt-6 text-xl text-gray-600">股票代碼</h2>
-
-        <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
-          <Link to={`/stock/${stock.id}`}>
-            <h2 className="text-2xl font-bold mb-2 text-gray-800">
-              {stock.id}
-            </h2>
-            <p className="text-gray-700">{stock.name}</p>
-          </Link>
-        </div>
-      </div>
-    );
-  });
+  return (
+    <div>
+      <h2 className="ml-7 mt-6 text-xl text-gray-600">股票代碼</h2>
+      {stocks.map((stock) => {
+        return (
+          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
+            <Link to={`/stock/${stock.id}`}>
+              <h2 className="text-2xl font-bold mb-2 text-gray-800">
+                {stock.id}
+              </h2>
+              <p className="text-gray-700">{stock.name}</p>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Stock;
